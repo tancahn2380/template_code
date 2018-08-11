800fd8a26b952085e1ce976e714d21d0ab481f06
tancahn <takuma70420@gmail.com>
2018-08-11 12:26:43 +0900

    オイラーツアーのテンプレを追加

---
+++ Eulor tour.cpp
@@ -0,0 +1,30 @@
+#include "bits/stdc++.h"
+
+//オイラーツアー
+//木を根からDFSするときに通過する頂点を一直線に並べたもの
+//Begin[i],End[i]はiを根とした時の部分木のオイラーツアーのindexを表す
+
+const int MAX_N = 100100
+vector<int> g[MAX_N];
+vector<int> euler_tour;
+int Begin[2 * MAX_N], End[2 * MAX_N];
+int k = 0, root = 0;
+void eulor(int v, int p){
+	Begin[v] = k;
+	euler_tour.emplace_back(v);
+	k++;
+	for (int i = 0; i<g[v].size(); i++)
+	{
+		if (g[v][i] != p)
+		{
+			eulor(g[v][i], v);
+			euler_tour.emplace_back(v);
+			k++;
+		}
+	}
+	End[v] = k;
+}
+
+int main(){
+	eulor(root, -1);
+}
