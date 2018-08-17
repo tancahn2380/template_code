b5d7fcf07d01bddb4f3caa489ec4d7bd1810bdce
tancahn <takuma70420@gmail.com>
2018-08-17 18:43:53 +0900

    BITの修正

--- BIT.cpp
+++ BIT.cpp
@@ -7,7 +7,7 @@ using namespace std;
 struct BIT {
 	vector<LL> bit;
 	BIT(int n) {
-		bit.resize(n);
+		bit.resize(n+10);
 		for (int i = 0; i < n; i++)bit[i] = 0;
 	}
 	int sum(int i) {
@@ -19,7 +19,7 @@ struct BIT {
 		return s;
 	}
 	void add(int i, LL x) {
-		while (i <= bit.size()) {
+		while (i <= bit.size()-1) {
 			bit[i] += x;
 			i += i& -i;
 		}
