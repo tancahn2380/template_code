e65c1a04982b3dbe552ff044dfa125665f5657de
tancahn <takuma70420@gmail.com>
2018-08-25 09:25:37 +0900

    BITを0-indexedにした

--- BIT.cpp
+++ BIT.cpp
@@ -2,6 +2,32 @@
 using namespace std;
 
 //BIT
+
+//0-indexed
+
+struct BIT {
+	vector<LL> bit;
+	BIT(LL n) {
+		bit.resize(n + 10);
+		for (int i = 0; i < n + 10; i++)bit[i] = 0;
+	}
+	//[0,i)
+	int sum(int i) {
+		LL s = 0;
+		for (int x = i - 1; x >= 0; x = (x&(x + 1)) - 1) {
+			s += bit[x];
+		}
+		return s;
+	}
+	void add(int a, LL w) {
+		for (int x = a; x < bit.size(); x |= x + 1) {
+			bit[x] += w;
+		}
+	}
+};
+
+
+
 //1-indexdであることに注意する
 
 struct BIT {
