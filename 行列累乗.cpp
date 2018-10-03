88ca078f42ebcc4bed0c812c4f2c7cdc320701f0
tancahn <takuma70420@gmail.com>
2018-10-03 14:06:41 +0900

    modを個別で指定できるようにする

--- 行列累乗.cpp
+++ 行列累乗.cpp
@@ -3,18 +3,16 @@ using namespace std;
 using LL = long long;
 
 //行列は二次元vectorを用いることにする
-typedef vector<int> vec;
+typedef vector<LL> vec;
 typedef vector<vec> mat;
 
-const int mod = 1e9 + 7;
-
 //A*Bの計算
-mat mul(mat &A, mat&B) {
+mat mul(mat &A, mat&B, int M) {
 	mat C(A.size(), vec(B[0].size()));
-	for (int i = 0; i < A.size();i++) {
+	for (int i = 0; i < A.size(); i++) {
 		for (int k = 0; k < B.size(); k++) {
 			for (int j = 0; j < B[0].size(); j++) {
-				C[i][j] = (C[i][j] + A[i][k] * B[k][j]) % mod;
+				C[i][j] = (C[i][j] + A[i][k] * B[k][j]) % M;
 			}
 		}
 	}
@@ -22,14 +20,14 @@ mat mul(mat &A, mat&B) {
 }
 
 //A^nの計算
-mat pow(mat A, LL n) {
+mat pow(mat A, LL n, int M) {
 	mat B(A.size(), vec(A.size()));
-	for (int i = 0; i < A.size();i++) {
+	for (int i = 0; i < A.size(); i++) {
 		B[i][i] = 1;
 	}
 	while (n > 0) {
-		if (n & 1)B = mul(B, A);
-		A = mul(A, A);
+		if (n & 1)B = mul(B, A, M);
+		A = mul(A, A, M);
 		n >>= 1;
 	}
 	return B;
@@ -42,6 +40,6 @@ int main() {
 	mat A(2, vec(2));
 	A[0][0] = 1, A[0][1] = 1;
 	A[1][0] = 1, A[1][1] = 0;
-	A = pow(A, n);
+	A = pow(A, n,1e9+7);
 	cout << A[1][0] << endl;
 }
