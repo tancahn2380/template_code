c40b41076bc80fb647973b9bd2ad3b5d93686d33
tancahn <takuma70420@gmail.com>
2018-11-08 00:15:31 +0900

    LCAの変更

--- LCA.cpp
+++ LCA.cpp
@@ -1,62 +1,85 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-//LCA
-//木の2頂点の祖先を求める
-//N,rootの定義に注意
+struct LCA {
+private:
+	int n;
+	vector<vector<int>>g;
+	int root;
+	vector<int>depth;
+	vector<vector<int>>par;
+public:
 
-int N, root;
+	void dfs(int v, int p, int d) {
+		par[v][0] = p;
+		depth[v] = d;
+		for (int i = 0; i<g[v].size(); i++) {
+			if (g[v][i] == p)continue;
+			dfs(g[v][i], v, d + 1);
+		}
+	}
+
+	LCA(int N,vector<int>* G,int Root) {
+		n = N;
+		g.resize(N);
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < G[i].size(); j++) {
+				g[i].emplace_back(G[i][j]);
+			}
+		}
+		depth.resize(N);
+		par.resize(N);
+		for (int i = 0; i < N; i++)par[i].resize(31);
+		root = Root;
+		dfs(root, -1, 0);
+		for (int i = 0; i < 30; i++) {
+			for (int j = 0; j < N; j++) {
+				if (par[j][i] == -1)par[j][i + 1] = -1;
+				else par[j][i + 1] = par[par[j][i]][i];
+			}
+		}
+	}
+
+	int get(int u, int v) {
+		if (depth[u] > depth[v])swap(u, v);
+		for (int i = 30; i >= 0; i--) {
+			if (((depth[v] - depth[u]) >> i) & 1) {
+				v = par[v][i];
+			}
+		}
+		if (u == v)return u;
+
+		for (int i = 30; i >= 0; i--) {
+			if (par[u][i] != par[v][i]) {
+				u = par[u][i];
+				v = par[v][i];
+			}
+		}
+		return par[u][0];
+	}
+};
+
+int n;
 vector<int> g[101010];
-int depth[101010];
-int par[101010][30];
 
-//木の深さを求める
-void dfs(int v, int p, int d){
-	par[v][0] = p;
-	depth[v] = d;
-	for (int i = 0; i<g[v].size(); i++){
-		if (g[v][i] == p)continue;
-		dfs(g[v][i], v, d + 1);
-	}
-}
-
-//par[v][i]:=頂点vから2^i回親をたどった頂点（無かったら-1）
-//vの親→par[v][0],vの親の親par[v][1]
-//par[v][i+1]=par[par[v][i]]よりpar[~][i]が計算できればpar[~][i+1]も求まる
-void fill_table(){
-	for (int i = 0; i<19; i++) {
-		for (int j = 0; j<N; j++){
-			if (par[j][i] == -1)par[j][i + 1] = -1;
-			else par[j][i + 1] = par[par[j][i]][i];
+int main() {
+	cin >> n;
+	REP(i, n) {
+		int k;
+		cin >> k;
+		REP(j, k) {
+			int c;
+			cin >> c;
+			g[i].emplace_back(c);
+			g[c].emplace_back(i);
 		}
 	}
-}
-
-
-//頂点u,vのLCAを求める
-int lca(int u, int v){
-
-	if (depth[u]>depth[v])swap(u, v);
-	//深さをそろえるための処理
-	for (int i = 19; i >= 0; i--) {
-		if (((depth[v] - depth[u]) >> i) & 1) {
-			v = par[v][i];
-		}
+	LCA lca(n, g, 0);
+	int q;
+	cin >> q;
+	REP(qqq, q) {
+		int u, v;
+		cin >> u >> v;
+		cout << lca.get(u, v) << endl;
 	}
-
-	if (u == v)return u;
-	//ダブリング
-	for (int i = 19; i >= 0; i--){
-		if (par[u][i] != par[v][i]){
-			u = par[u][i];
-			v = par[v][i];
-		}
-	}
-	return par[u][0];
-}
-
-int main(){
-	dfs(root, -1, 0);
-	fill_table();
-	return 0;
 }
