e564a9e1d91bb697a96822021d68afdd24b76843
tancahn2380 <takuma70420@gmail.com>
2018-12-17 21:52:15 +0900

    union-find木に集合のサイズを求める関数を追加

--- union-find-tree.cpp
+++ union-find-tree.cpp
@@ -1,99 +1,33 @@
 #include "bits/stdc++.h"
-
 using namespace std;
 
 struct UnionFind {
-	vector<int>Par, Rank;
-	UnionFind(int n) {
-		Par.resize(n);
-		Rank.resize(n);
-		for (int i = 0; i < n; i++) {
-			Par[i] = i;
-			Rank[i] = 0;
-		}
-	}
+	UnionFind(size_t size) : v(size, -1) {}
+	vector<int> v;
 
-	int find(int x) {
-		if (Par[x] == x) {
-			return x;
-		}
-		else {
-			return Par[x] = find(Par[x]);
-		}
-	}
-
-	void unite(int  x, int y) {
-		x = find(x);
-		y = find(y);
-		if (x == y) return;
-		if (Rank[x] < Rank[y]) {
-			Par[x] = y;
-		}
-		else {
-			Par[y] = x;
-			if (Rank[x] == Rank[y])Rank[x]++;
-		}
-	}
-
-	bool same(int x, int y) {
-		return find(x) == find(y);
-	}
-};
-
-int main() {
-	int n;
-	cin >> n;
-	UnionFind uf(n);
-	int q;
-	cin >> q;
-	REP(i, q) {
-		int a, b, c;
-		cin >> a >> b >> c;
-		if (a == 0)uf.unite(b, c);
-		else cout << uf.same(b, c) << endl;
-	}
+int root(int x) {
+    return (v[x] < 0 ? x : v[x] = root(v[x]));
 }
 
+bool is_root(int x) {
+    return x == root(x);
+}
 
-//satanic0258
-/*
-//Union-Find
-class UnionFind {
-private:
-	std::vector<int> parent;
-	std::vector<int> height;
-	std::vector<int> m_size;
+bool same(int x, int y) {
+    return root(x) == root(y);
+}
 
-public:
-	UnionFind(int size_) : parent(size_), height(size_, 0), m_size(size_, 1) {
-		for (int i = 0; i < size_; ++i) parent[i] = i;
-	}
-	void init(int size_) {
-		parent.resize(size_);
-		height.resize(size_, 0);
-		m_size.resize(size_, 0);
-		for (int i = 0; i < size_; ++i) parent[i] = i;
-	}
-	int find(int x) {
-		if (parent[x] == x) return x;
-		return parent[x] = find(parent[x]);
-	}
-	void unite(int x, int y) {
-		x = find(x);
-		y = find(y);
-		if (x == y) return;
-		int t = size(x) + size(y);
-		m_size[x] = m_size[y] = t;
-		if (height[x] < height[y]) parent[x] = y;
-		else parent[y] = x;
-		if (height[x] == height[y]) ++height[x];
-	}
-	bool same(int x, int y) {
-		return find(x) == find(y);
-	}
-	int size(int x) {
-		if (parent[x] == x) return m_size[x];
-		return size(parent[x] = find(parent[x]));
+void unite(int x, int y) {
+    x = root(x);
+    y = root(y);
+    if (x != y) {
+		if (v[x] > v[y]) swap(x, y);
+		v[x] += v[y];
+		v[y] = x;
+    }
+}
+
+int size(int x) {
+    	return -v[root(x)];
 	}
 };
-*/
