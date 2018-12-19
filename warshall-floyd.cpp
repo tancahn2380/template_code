dcc480b1bfbbd180875f2630b017c3ee04b12caf
tancahn2380 <takuma70420@gmail.com>
2018-12-19 21:31:26 +0900

    ワーシャルフロイド法の変更

--- warshall-floyd.cpp
+++ warshall-floyd.cpp
@@ -2,18 +2,12 @@
 using LL = long long;
 using namespace std;
 template<class T>constexpr T INF() { return ::std::numeric_limits<T>::max(); }
-
-
-const int MAX_V=10; 
+template<class T>constexpr T HINF() { return INF<T>() / 2; }
+const int MAX_V=10;
 LL d[MAX_V][MAX_V];//d[i][j]はe={u,v}のコスト
 int V;
 
 void warshall_floyd(){
-	for (int i = 0; i < V; i++) {
-		for (int j = 0; j < V; j++) {
-			d[i][j] = INF;
-		}
-	}
 	for(int k=0;k<V;k++){
 		for(int i=0;i<V;i++){
 			for(int j=0;j<V;j++){
