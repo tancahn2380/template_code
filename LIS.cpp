3034e6cbb28a21676234289af44da1c3aca7a671
tancahn2380 <takuma70420@gmail.com>
2018-12-27 17:11:15 +0900

    エディタ上でのエラーが厄介だったので修正

--- LIS.cpp
+++ LIS.cpp
@@ -1,5 +1,7 @@
 #include "bits/stdc++.h"
+using LL = long long;
 template<class T>constexpr T INF() { return ::std::numeric_limits<T>::max(); }
+template<class T>constexpr T HINF() { return INF<T>() / 2; }
 using namespace std;
 
 constexpr int MAX_N = 100000;
@@ -13,11 +15,11 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		cin >> a[i];
 	}
-	fill(dp, dp + n, INF);
+	fill(dp, dp + n, INF<int>());
 	for (int i = 0; i < n; i++) {
 		*lower_bound(dp, dp + n, a[i]) = a[i];
 	}
-	cout << lower_bound(dp, dp + n, INF) - dp << endl;
+	cout << lower_bound(dp, dp + n, INF<int>()) - dp << endl;
 }
 
 int lis(vector<LL> &v) {
@@ -30,7 +32,7 @@ int lis(vector<LL> &v) {
 
 //広義短調増加
 
-int lis(vector<LL> &v) {
+int lis2(vector<LL> &v) {
 	vector<LL> ans(v.size(), HINF<LL>());
 	for (int i = 0; i < v.size(); i++) {
 		(*upper_bound(ans.begin(), ans.end(), v[i])) = v[i];
