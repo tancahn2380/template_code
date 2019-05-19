2e46f94e38eb6adfa7d6e8dc5de5db1e3e924d13
tancahn2380 <takuma70420@gmail.com>
2019-05-19 14:11:58 +0900

    経路圧縮ありのUnion-Findの追加

--- union-find-tree.cpp
+++ union-find-tree.cpp
@@ -1,6 +1,9 @@
 #include "bits/stdc++.h"
 using namespace std;
+template<class T>constexpr T INF() { return ::std::numeric_limits<T>::max(); }
+template<class T>constexpr T HINF() { return INF<T>() / 2; }
 
+//経路圧縮なし、サイズを求めるとき以外はあまり使わないほうが良い、ならしO(log N)
 struct UnionFind {
 	UnionFind(size_t size) : v(size, -1) {}
 	vector<int> v;
@@ -30,4 +33,70 @@ struct UnionFind {
     int size(int x) {
     	return -v[root(x)];
 	}
+};
+
+//部分永続、経路圧縮無し、ならしO(log N)
+struct UnionFind {
+	UnionFind(size_t size){
+        for(int i = 0;i < (int)size;i++){
+            par.emplace_back(-1);
+            time.emplace_back(HINF<int>());
+        }
+        now = 0;
+    }
+	vector<int> par, time;
+    int now;
+
+    int find(int t, int x){
+        if(time[x] > t)return x;
+        else if(par[x] < 0)return x;
+        else return find(t, par[x]);
+    }
+
+    bool same(int t, int x, int y){
+        return find(t, x) == find(t, y);
+    }
+
+    int unite(int x, int y){
+        ++now;
+        x = find(now, x);
+        y = find(now, y);
+        if(x == y)return now;
+        if(par[x] < par[y])swap(x, y);
+        par[x] += par[y];
+        par[y] = x;
+        time[y] = now;
+        return now;
+    }
+};
+
+//経路圧縮あり、O(α(N))
+struct UnionFind {
+	UnionFind(size_t size){
+        for(int i = 0;i < (int)size;i++){
+            par.emplace_back(i);
+            rnk.emplace_back(0);
+        }
+    }
+	vector<int> par, rnk;
+
+    int find(int x){
+        if(par[x] == x)return x;
+        else return par[x] = find(par[x]);
+    }
+
+    void unite(int x, int y){
+        x = find(x);
+        y = find(y);
+        if(x == y)return;
+        if(rnk[x] < rnk[y]){
+            par[x] = y;
+        }else{
+            par[y] = x;
+            if(rnk[x] == rnk[y])rnk[x]++;
+        }
+    }
+    bool same(int x,int y){
+        return find(x) == find(y);
+    }
 };
