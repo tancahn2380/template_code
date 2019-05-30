53442ef70a731dc3a022f99d9f4e07f7981ed4cf
tancahn2380 <takuma70420@gmail.com>
2019-05-30 21:00:11 +0900

    BITの操作を開区間でできるようにした

--- BIT.cpp
+++ BIT.cpp
@@ -5,14 +5,13 @@ using namespace std;
 //BIT
 
 //0-indexed
-
 struct BIT {
 	vector<LL> bit;
 	BIT(LL n) {
 		bit.resize(n + 10);
-		for (int i = 0; i < n + 10; i++)bit[i] = 0;
 	}
-	//[0,i)
+
+	//[0, i)
 	LL sum(int i) {
 		LL s = 0;
 		for (int x = i - 1; x >= 0; x = (x&(x + 1)) - 1) {
@@ -20,35 +19,56 @@ struct BIT {
 		}
 		return s;
 	}
+
+    //[l, r)
+    LL sum(int l, int r){
+        return sum(r) - sum(l);
+    }
+
+    //[0, i]
+    LL sum_open(int i) {
+		LL s = 0;
+		for (int x = i; x >= 0; x = (x&(x + 1)) - 1) {
+			s += bit[x];
+		}
+		return s;
+	}
+
+    //[l, r]
+    LL sum_open(int l, int r){
+        return sum_open(r) - sum_open(l - 1);
+    }
 	void add(int a, LL w) {
 		for (int x = a; x < (int)bit.size(); x |= x + 1) {
 			bit[x] += w;
 		}
 	}
-};
 
+    //indexを返す
+    int lower_bound(LL x){
+        int l = -1, r = (int)bit.size();
+        while(r - l > 1){
+            int mid = (l + r)/2;
+            if(sum_open(mid) < x){
+                l = mid;
+            }else{
+                r = mid;
+            }
+        }
+        return r;
+    }
 
-
-//1-indexdであることに注意する
-
-struct BIT {
-	vector<LL> bit;
-	BIT(int n) {
-		bit.resize(n+10);
-		for (int i = 0; i < n; i++)bit[i] = 0;
-	}
-	LL sum(int i) {
-		LL s = 0;
-		while (i>0) {
-			s += bit[i];
-			i -= i& -i;
-		}
-		return s;
-	}
-	void add(int i, LL x) {
-		while (i <= bit.size()-1) {
-			bit[i] += x;
-			i += i& -i;
-		}
-	}
+    //indexを返す
+    int upper_bound(LL x){
+        int l = -1, r = (int)bit.size();
+        while(r - l > 1){
+            int mid = (l + r)/2;
+            if(sum_open(mid) <= x){
+                l = mid;
+            }else{
+                r = mid;
+            }
+        }
+        return r;
+    }
 };
